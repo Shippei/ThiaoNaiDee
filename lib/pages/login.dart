0ea617825a8b3d540581988b7984949cb496906c
Shippei <53250781+Shippei@users.noreply.github.com>
2020-12-15 15:05:25 +0700

    edit UI login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 // ignore: must_be_immutable
 class LoginPage extends StatelessWidget {
+  final Color secondaryColor = Color(0xff232c51);
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  // ignore: unused_field
   String message;
-  TextEditingController _usernameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -19,22 +23,16 @@ class LoginPage extends StatelessWidget {
                 height: 200,
                 width: 200,
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(60, 10, 60, 0),
-                child: TextField(
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(labelText: 'บัญชีผู้ใช้'),
-                  controller: _usernameController,
-                ),
+              SizedBox(
+                height: 30,
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(60, 0, 60, 10),
-                child: TextField(
-                  textAlign: TextAlign.center,
-                  obscureText: true,
-                  decoration: InputDecoration(labelText: 'รหัสผ่าน'),
-                ),
+              _buildTextField(
+                  nameController, Icons.account_circle, 'บัญชีผู้ใช้', false),
+              SizedBox(
+                height: 20,
               ),
+              _buildTextField(passwordController, Icons.lock, 'รหัสผ่าน', true),
+              SizedBox(height: 30),
               Padding(
                 padding: const EdgeInsets.fromLTRB(0, 0, 60, 0),
                 child: Row(
@@ -77,9 +75,53 @@ class LoginPage extends StatelessWidget {
                   ],
                 ),
               ),
+              SizedBox(height: 10),
+              MaterialButton(
+                elevation: 0,
+                minWidth: double.maxFinite,
+                height: 50,
+                onPressed: () {
+                  //Here goes the logic for Google SignIn discussed in the next section
+                },
+                color: Colors.blue,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(FontAwesomeIcons.google),
+                    SizedBox(width: 10),
+                    Text('Sign-in using Google',
+                        style: TextStyle(color: Colors.white, fontSize: 16)),
+                  ],
+                ),
+                textColor: Colors.white,
+              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  _buildTextField(TextEditingController controller, IconData icon,
+      String labelText, bool a) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      decoration: BoxDecoration(
+          color: secondaryColor, border: Border.all(color: Colors.blue)),
+      child: TextField(
+        controller: controller,
+        obscureText: a,
+        style: TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(horizontal: 10),
+            labelText: labelText,
+            labelStyle: TextStyle(color: Colors.white),
+            icon: Icon(
+              icon,
+              color: Colors.white,
+            ),
+            // prefix: Icon(icon),
+            border: InputBorder.none),
       ),
     );
   }
